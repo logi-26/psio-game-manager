29b38e9adb968453d088cf96ec5978272ad15131
logi-26 <logi.dev.backup@gmail.com>
2025-09-06 18:29:33 +0100

    refactor: Re-write the bin merging code

--- src/binmerge.py
+++ src/binmerge.py
@@ -1,271 +1,112 @@
-#!/usr/bin/env python3
-#
-#  binmerge
-#
-#  Takes a cue sheet with multiple binary track files and merges them together,
-#  generating a corrected cue sheet in the process.
-#
-#  Copyright (C) 2020 Chris Putnam
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
-#
-#  This code has been modified by LoGi26 (2021) for use with the psio-assist script
+'''
+Merges multiple BIN files into a single BIN file and generates a new CUE file
+'''
 
-from os import access, R_OK, name
-from os.path import exists, join, dirname, isfile, getsize
-from re import search, match
-from typing import List, Union
-from shutil import copyfileobj
-import subprocess
-
-# Global variables
-ERROR_LOG_PATH = None
+# System imports
+from os.path import exists, join
 
 
-# ************************************************************************************
-class Track:
-    """A track within a binary file"""
-    globalBlocksize = None
-
-    def __init__(self, num, track_type):
-        self.num = num
-        self.indexes = []
-        self.track_type = track_type
-        self.sectors = None
-        self.file_offset = None
-
-        # All possible blocksize types. You cannot mix types on a disc, so we will use the first one we see and lock it in.
-        if not Track.globalBlocksize:
-            if track_type in ['AUDIO', 'MODE1/2352', 'MODE2/2352', 'CDI/2352']:
-                Track.globalBlocksize = 2352
-            elif track_type == 'CDG':
-                Track.globalBlocksize = 2448
-            elif track_type == 'MODE1/2048':
-                Track.globalBlocksize = 2048
-            elif track_type in ['MODE2/2336', 'CDI/2336']:
-                Track.globalBlocksize = 2336
-# ************************************************************************************
+class BinMerger:
+    """A class to handle merging BIN files and creating a merged CUE sheet for a game"""
+    def __init__(self, debug_mode: bool = False):
+        """Initialise the BinMerger"""
+        self.debug_mode = debug_mode
 
 
-# ************************************************************************************
-class File:
-    """A binary file with its associated tracks and indexes"""
-    def __init__(self, filename):
-        self.filename = filename
-        self.tracks = []
-        self.size = getsize(filename)
-# ************************************************************************************
+    # ************************************************************************************
+    def _debug_print(self, message: str):
+        """Print debug messages if debug mode is enabled"""
+        if self.debug_mode:
+            print(message)
+    # ************************************************************************************
 
 
-# ************************************************************************************
-class BinFilesMissingException(Exception):
-    """Exception raised when one or more binary files referenced in the cue sheet are missing"""
-    pass
-# ************************************************************************************
+    # ************************************************************************************
+    def _sectors_to_cue_stamp(self, sectors: int):
+        """Convert sectors to a CUE file timestamp (MM:SS:FF)"""
+        minutes = sectors / 4500
+        fields = sectors % 4500
+        seconds = fields / 75
+        fields = sectors % 75
+        return '%02d:%02d:%02d' % (minutes, seconds, fields)
+    # ************************************************************************************
 
 
-# ************************************************************************************
-def _sectors_to_cuestamp(sectors):
-    """Convert sectors to a cue sheet timestamp (MM:SS:FF)"""
-    minutes = sectors / 4500
-    fields = sectors % 4500
-    seconds = fields / 75
-    fields = sectors % 75
-    return '%02d:%02d:%02d' % (minutes, seconds, fields)
-# ************************************************************************************
+    # ************************************************************************************
+    def _create_merged_cuesheet(self, game_name: str, bin_files: list) -> str:
+        """Creates a merged CUE file containing the multiple tracks"""
+        cue_sheet = f'FILE "{game_name}.bin" BINARY\n'
+
+        # One sector is (BLOCKSIZE) bytes
+        sector_pos = 0
+        for bin_file in bin_files:
+            for track in bin_file.get_tracks():
+                cue_sheet += f'   TRACK {track.get_track_number()} {track.get_track_type()}\n'
+
+                for index in track.get_indexes():
+                    sectors_to_cue_stamp = self._sectors_to_cue_stamp(sector_pos + index["file_offset"])
+                    cue_sheet += f'   INDEX {index["id"]} {sectors_to_cue_stamp}\n'
+                sector_pos += bin_file.get_size() / track.get_block_size()
+
+        return cue_sheet
+    # ************************************************************************************
 
 
-# ************************************************************************************
-def _cuestamp_to_sectors(stamp):
-    """Convert a cue sheet timestamp (MM:SS:FF) to sectors"""
-    m = match('(\d+):(\d+):(\d+)', stamp)
-    minutes = int(m.group(1))
-    seconds = int(m.group(2))
-    fields = int(m.group(3))
-    return fields + (seconds * 75) + (minutes * 60 * 75)
-# ************************************************************************************
+    # ************************************************************************************
+    def _merge_files(self, merged_bin_path: str, bin_files: list) -> bool:
+        """Merges a list of BIN files into a single file"""
 
+        if exists(merged_bin_path):
+            self._debug_print(f"(Error) Target merged file already exists: {merged_bin_path}")
+            return False
 
-# ************************************************************************************
-def _gen_merged_cuesheet(basename, files):
-    """generates a 'merged' cue sheet, that is, one bin file with tracks indexed within"""
-    cue_sheet = f'FILE "{basename}.bin" BINARY\n'
-
-    # One sector is (BLOCKSIZE) bytes
-    sector_pos = 0
-    for f in files:
-        for t in f.tracks:
-            cue_sheet += f'   TRACK {t.num} {t.track_type}\n'
-            for i in t.indexes:
-                cue_sheet += f'   INDEX {i["id"]} {_sectors_to_cuestamp(sector_pos + i["file_offset"])}\n'
-            sector_pos += f.size / Track.globalBlocksize
-
-    return cue_sheet
-# ************************************************************************************
-
-
-# ************************************************************************************
-def _merge_files(merged_filename: str, files: List[Union[str, object]], use_native: bool = True, memory_merge: bool = False) -> bool:
-    """Merge multiple binary files into a single output file"""
-
-    # Validate target file
-    if exists(merged_filename):
-        print(f"Error: Target merged file already exists: {merged_filename}")
-        raise FileExistsError(f"Target merged file already exists: {merged_filename}")
-
-    # Validate and collect file paths
-    file_paths = []
-    for f in files:
-        path = f.filename if hasattr(f, 'filename') else f
-        if not isfile(path):
-            print(f"Error: Input file does not exist or is not a file: {path}")
-            raise FileNotFoundError(f"Input file does not exist or is not a file: {path}")
-        file_paths.append(path)
-
-    try:
-        if use_native:
-            # Use native OS commands for fastest merging
-            if name == 'nt':  	# Windows
-                cmd = 'copy /b ' + ' + '.join(f'"{path}"' for path in file_paths) + f' "{merged_filename}"'
-                subprocess.run(cmd, shell=True, check=True)
-            else:  				# Unix/Linux/macOS
-                cmd = ['cat'] + file_paths + ['>', merged_filename]
-                subprocess.run(' '.join(cmd), shell=True, check=True)
-        else:
-            # Fallback to memory-based or file-based merging
-            if memory_merge:
-                # Pre-read all files into memory (fast for small audio tracks)
-                data = bytearray()
-                for file_path in file_paths:
-                    with open(file_path, 'rb') as in_file:
-                        data.extend(in_file.read())
-                with open(merged_filename, 'wb') as out_file:
-                    out_file.write(data)
-            else:
-                # Sequential merging with shutil (faster than chunk-based)
-                with open(merged_filename, 'wb') as out_file:
-                    for file_path in file_paths:
-                        with open(file_path, 'rb') as in_file:
-                            copyfileobj(in_file, out_file)
+        chunk_size = 1024 * 1024
+        with open(merged_bin_path, 'wb') as outfile:
+            for bin_file in bin_files:
+                with open(bin_file.get_file_path(), 'rb') as in_file:
+                    self._debug_print(f"Merging BIN file: {bin_file.get_file_name()}")
+                    while True:
+                        chunk = in_file.read(chunk_size)
+                        if not chunk:
+                            break
+                        outfile.write(chunk)
 
         return True
-
-    except (subprocess.CalledProcessError, IOError, OSError) as error:
-        print(f"Error merging files: {error}")
-        return False
-# ************************************************************************************
+    # ************************************************************************************
 
 
-# ************************************************************************************
-def _log_error(error_type, error_message):
-    """Log error messages to a file if the error log path is set"""
-    if ERROR_LOG_PATH is not None:
-        try:
-            with open(ERROR_LOG_PATH, 'a+', encoding='utf-8') as error_log_file:
-                error_log_file.write(f'[{error_type}]: {error_message}\n')
-        except IOError:
-            with open(ERROR_LOG_PATH, 'w', encoding='utf-8') as error_log_file:
-                error_log_file.write(f'[{error_type}]: {error_message}\n')
-# ************************************************************************************
+    # ************************************************************************************
+    def merge(self, game_name: str, cue_file_name: str, bin_files: list, out_dir: str) -> bool:
+        """Main method to start the bin merging process"""
+        self._debug_print("\nMerging BIN files...")
 
+        # Return if the output directory does not exist
+        if not exists(out_dir):
+            self._debug_print(f'(ERROR) Output dir does not exist: {out_dir}')
+            return False
 
-# ************************************************************************************
-def set_binmerge_error_log_path(log_path):
-    """Set the path for the error log file"""
-    global ERROR_LOG_PATH
-    ERROR_LOG_PATH = log_path
-# ************************************************************************************
+        merged_cue_path = join(out_dir, cue_file_name)
 
+        # Return if a merged CUE file already exists
+        if exists(merged_cue_path):
+            self._debug_print(f'(ERROR) Output CUE file already exists: {merged_cue_path}')
+            return False
 
-# ************************************************************************************
-def read_cue_file(cue_path):
-    """Read and parse a cue file, returning a list of File objects with their tracks and indexes"""
-    files = []
-    this_track = None
-    this_file = None
-    bin_files_missing = False
+        # Create the merged CUE file content
+        cue_sheet = self._create_merged_cuesheet(game_name, bin_files)
 
-    f = open(cue_path, 'r', encoding='utf-8')
-    for line in f:
-        m = search('FILE "?(.*?)"? BINARY', line)
-        if m:
-            this_path = join(dirname(cue_path), m.group(1))
-            file_available = (isfile(this_path) or access(this_path, R_OK))
+        # Merge the BIN files
+        merged_bin_path = join(out_dir, game_name + '.bin')
 
-            if not file_available:
-                this_path = join(dirname(cue_path), m.group(1).replace(' (Track 01)', ''))
-                file_available = (isfile(this_path) or access(this_path, R_OK))
-                if not file_available:
-                    this_path = join(dirname(cue_path), m.group(1).replace(' (Track 1)', ''))
-                    file_available = (isfile(this_path) or access(this_path, R_OK))
+        if not self._merge_files(merged_bin_path, bin_files):
+            return False
 
-            if not file_available:
-                bin_files_missing = True
-            else:
-                this_file = File(this_path)
-                files.append(this_file)
+        # Write the merged CUE file
+        self._debug_print("Creating merged CUE file...")
+        with open(merged_cue_path, 'w', encoding='utf-8', newline='\r\n') as f:
+            f.write(cue_sheet)
 
-            continue
+        self._debug_print("BIN files have been merged")
 
-        m = search('TRACK (\d+) ([^\s]*)', line)
-        if m and this_file:
-            this_track = Track(int(m.group(1)), m.group(2))
-            this_file.tracks.append(this_track)
-            continue
-
-        m = search('INDEX (\d+) (\d+:\d+:\d+)', line)
-        if m and this_track:
-            this_track.indexes.append({'id': int(m.group(1)), 'stamp': m.group(2), 'file_offset':_cuestamp_to_sectors(m.group(2))})
-            continue
-
-    if bin_files_missing:
-        _log_error('ERROR', f'file does not exist: {line}')
-        return []
-
-    if len(files) == 1:
-        # only 1 file, assume splitting, calc sectors of each
-        next_item_offset = files[0].size // Track.globalBlocksize
-        for t in reversed(files[0].tracks):
-            t.sectors = next_item_offset - t.indexes[0]["file_offset"]
-            next_item_offset = t.indexes[0]["file_offset"]
-
-    return files
-# ************************************************************************************
-
-
-# ************************************************************************************
-def start_bin_merge(cue_file, game_name, out_dir):
-    """Main function to start the bin merging process"""
-    cue_map = read_cue_file(cue_file)
-    cue_sheet = _gen_merged_cuesheet(game_name, cue_map)
-
-    if not exists(out_dir):
-        _log_error('ERROR', 'Output dir does not exist')
-        return False
-
-    new_cue_fn = join(out_dir, game_name + '.cue')
-    if exists(new_cue_fn):
-        _log_error('ERROR', f'Output cue file already exists. Quitting. Path: {new_cue_fn}')
-        return False
-
-    if not _merge_files(join(out_dir, game_name + '.bin'), cue_map):
-        return False
-
-    with open(new_cue_fn, 'w', encoding='utf-8', newline='\r\n') as f:
-        f.write(cue_sheet)
-
-    return True
-# ************************************************************************************
+        return True
+    # ************************************************************************************
